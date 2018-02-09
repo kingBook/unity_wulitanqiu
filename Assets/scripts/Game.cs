c6aa3cc7e02a67bf5307230f6e884637f6e02158
kingBook <412126604@qq.com>
2018-02-09 17:05:52 +0800

    update 2018-02-09

---
+++ Assets/scripts/Game.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+	private void Start () {
+		
+	}
+	
+	private void Update () {
+		bool isMouseDown;
+		
+		if(Input.GetMouseButton(0)){
+			
+		}
+	}
+}
